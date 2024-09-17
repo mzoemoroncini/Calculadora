dc06d698191bb7d6256ba98b5d081dccfaa021c3
Usuario <Usuario@Z-ACER>
2024-09-17 20:30:28 -0300

    Agregar archivos de proyecto.

---
+++ Evaluacion3/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Evaluacion3
+{
+    partial class Calculadora
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Calculadora));
+            this.btsuma = new System.Windows.Forms.Button();
+            this.btresta = new System.Windows.Forms.Button();
+            this.btdividir = new System.Windows.Forms.Button();
+            this.btmultiplicar = new System.Windows.Forms.Button();
+            this.bt1 = new System.Windows.Forms.Button();
+            this.bt2 = new System.Windows.Forms.Button();
+            this.bt3 = new System.Windows.Forms.Button();
+            this.bt4 = new System.Windows.Forms.Button();
+            this.bt5 = new System.Windows.Forms.Button();
+            this.bt6 = new System.Windows.Forms.Button();
+            this.bt7 = new System.Windows.Forms.Button();
+            this.bt8 = new System.Windows.Forms.Button();
+            this.bt9 = new System.Windows.Forms.Button();
+            this.bt0 = new System.Windows.Forms.Button();
+            this.btcoma = new System.Windows.Forms.Button();
+            this.btborrartodo = new System.Windows.Forms.Button();
+            this.btborrar = new System.Windows.Forms.Button();
+            this.btigual = new System.Windows.Forms.Button();
+            this.txbcargar = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btsuma
+            // 
+            this.btsuma.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btsuma.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btsuma.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btsuma.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btsuma.Location = new System.Drawing.Point(15, 77);
+            this.btsuma.Name = "btsuma";
+            this.btsuma.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btsuma.Size = new System.Drawing.Size(46, 50);
+            this.btsuma.TabIndex = 0;
+            this.btsuma.Text = "+";
+            this.btsuma.UseVisualStyleBackColor = false;
+            this.btsuma.Click += new System.EventHandler(this.btsuma_Click);
+            // 
+            // btresta
+            // 
+            this.btresta.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btresta.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btresta.Location = new System.Drawing.Point(75, 77);
+            this.btresta.Name = "btresta";
+            this.btresta.Size = new System.Drawing.Size(46, 50);
+            this.btresta.TabIndex = 1;
+            this.btresta.Text = "-";
+            this.btresta.UseVisualStyleBackColor = false;
+            this.btresta.Click += new System.EventHandler(this.btresta_Click);
+            // 
+            // btdividir
+            // 
+            this.btdividir.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btdividir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btdividir.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btdividir.Location = new System.Drawing.Point(138, 77);
+            this.btdividir.Name = "btdividir";
+            this.btdividir.Size = new System.Drawing.Size(46, 50);
+            this.btdividir.TabIndex = 2;
+            this.btdividir.Text = "/";
+            this.btdividir.UseVisualStyleBackColor = false;
+            this.btdividir.Click += new System.EventHandler(this.btdividir_Click);
+            // 
+            // btmultiplicar
+            // 
+            this.btmultiplicar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btmultiplicar.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btmultiplicar.Location = new System.Drawing.Point(201, 77);
+            this.btmultiplicar.Name = "btmultiplicar";
+            this.btmultiplicar.Size = new System.Drawing.Size(46, 50);
+            this.btmultiplicar.TabIndex = 3;
+            this.btmultiplicar.Text = "*";
+            this.btmultiplicar.UseVisualStyleBackColor = false;
+            this.btmultiplicar.Click += new System.EventHandler(this.btmultiplicar_Click);
+            // 
+            // bt1
+            // 
+            this.bt1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt1.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt1.Location = new System.Drawing.Point(12, 139);
+            this.bt1.Name = "bt1";
+            this.bt1.Size = new System.Drawing.Size(57, 47);
+            this.bt1.TabIndex = 4;
+            this.bt1.Text = "1";
+            this.bt1.UseVisualStyleBackColor = false;
+            this.bt1.Click += new System.EventHandler(this.bt1_Click);
+            // 
+            // bt2
+            // 
+            this.bt2.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt2.Font = new System.Drawing.Font("Calisto MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt2.Location = new System.Drawing.Point(75, 139);
+            this.bt2.Name = "bt2";
+            this.bt2.Size = new System.Drawing.Size(57, 47);
+            this.bt2.TabIndex = 5;
+            this.bt2.Text = "2";
+            this.bt2.UseVisualStyleBackColor = false;
+            this.bt2.Click += new System.EventHandler(this.bt2_Click);
+            // 
+            // bt3
+            // 
+            this.bt3.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt3.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt3.Location = new System.Drawing.Point(138, 139);
+            this.bt3.Name = "bt3";
+            this.bt3.Size = new System.Drawing.Size(57, 47);
+            this.bt3.TabIndex = 6;
+            this.bt3.Text = "3";
+            this.bt3.UseVisualStyleBackColor = false;
+            this.bt3.Click += new System.EventHandler(this.bt3_Click);
+            // 
+            // bt4
+            // 
+            this.bt4.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt4.Font = new System.Drawing.Font("Calisto MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt4.Location = new System.Drawing.Point(12, 192);
+            this.bt4.Name = "bt4";
+            this.bt4.Size = new System.Drawing.Size(57, 47);
+            this.bt4.TabIndex = 7;
+            this.bt4.Text = "4";
+            this.bt4.UseVisualStyleBackColor = false;
+            this.bt4.Click += new System.EventHandler(this.bt4_Click);
+            // 
+            // bt5
+            // 
+            this.bt5.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt5.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt5.Location = new System.Drawing.Point(75, 192);
+            this.bt5.Name = "bt5";
+            this.bt5.Size = new System.Drawing.Size(57, 47);
+            this.bt5.TabIndex = 8;
+            this.bt5.Text = "5";
+            this.bt5.UseVisualStyleBackColor = false;
+            this.bt5.Click += new System.EventHandler(this.bt5_Click);
+            // 
+            // bt6
+            // 
+            this.bt6.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt6.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt6.Location = new System.Drawing.Point(138, 192);
+            this.bt6.Name = "bt6";
+            this.bt6.Size = new System.Drawing.Size(57, 47);
+            this.bt6.TabIndex = 9;
+            this.bt6.Text = "6";
+            this.bt6.UseVisualStyleBackColor = false;
+            this.bt6.Click += new System.EventHandler(this.bt6_Click);
+            // 
+            // bt7
+            // 
+            this.bt7.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt7.Font = new System.Drawing.Font("Calisto MT", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt7.Location = new System.Drawing.Point(12, 245);
+            this.bt7.Name = "bt7";
+            this.bt7.Size = new System.Drawing.Size(57, 47);
+            this.bt7.TabIndex = 10;
+            this.bt7.Text = "7";
+            this.bt7.UseVisualStyleBackColor = false;
+            this.bt7.Click += new System.EventHandler(this.bt7_Click);
+            // 
+            // bt8
+            // 
+            this.bt8.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt8.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt8.Location = new System.Drawing.Point(75, 245);
+            this.bt8.Name = "bt8";
+            this.bt8.Size = new System.Drawing.Size(57, 47);
+            this.bt8.TabIndex = 11;
+            this.bt8.Text = "8";
+            this.bt8.UseVisualStyleBackColor = false;
+            this.bt8.Click += new System.EventHandler(this.bt8_Click);
+            // 
+            // bt9
+            // 
+            this.bt9.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt9.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt9.Location = new System.Drawing.Point(138, 245);
+            this.bt9.Name = "bt9";
+            this.bt9.Size = new System.Drawing.Size(57, 47);
+            this.bt9.TabIndex = 12;
+            this.bt9.Text = "9";
+            this.bt9.UseVisualStyleBackColor = false;
+            this.bt9.Click += new System.EventHandler(this.bt9_Click);
+            // 
+            // bt0
+            // 
+            this.bt0.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.bt0.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt0.Location = new System.Drawing.Point(12, 298);
+            this.bt0.Name = "bt0";
+            this.bt0.Size = new System.Drawing.Size(120, 47);
+            this.bt0.TabIndex = 13;
+            this.bt0.Text = "0";
+            this.bt0.UseVisualStyleBackColor = false;
+            this.bt0.Click += new System.EventHandler(this.bt0_Click);
+            // 
+            // btcoma
+            // 
+            this.btcoma.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btcoma.Font = new System.Drawing.Font("Calisto MT", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btcoma.Location = new System.Drawing.Point(138, 298);
+            this.btcoma.Name = "btcoma";
+            this.btcoma.Size = new System.Drawing.Size(57, 47);
+            this.btcoma.TabIndex = 14;
+            this.btcoma.Text = ",";
+            this.btcoma.UseVisualStyleBackColor = false;
+            this.btcoma.Click += new System.EventHandler(this.btcoma_Click);
+            // 
+            // btborrartodo
+            // 
+            this.btborrartodo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btborrartodo.Font = new System.Drawing.Font("Calisto MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btborrartodo.Location = new System.Drawing.Point(201, 139);
+            this.btborrartodo.Name = "btborrartodo";
+            this.btborrartodo.Size = new System.Drawing.Size(57, 47);
+            this.btborrartodo.TabIndex = 15;
+            this.btborrartodo.Text = "BT";
+            this.btborrartodo.UseVisualStyleBackColor = false;
+            this.btborrartodo.Click += new System.EventHandler(this.btborrartodo_Click);
+            // 
+            // btborrar
+            // 
+            this.btborrar.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btborrar.Font = new System.Drawing.Font("Calisto MT", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btborrar.Location = new System.Drawing.Point(201, 192);
+            this.btborrar.Name = "btborrar";
+            this.btborrar.Size = new System.Drawing.Size(57, 47);
+            this.btborrar.TabIndex = 16;
+            this.btborrar.Text = "B";
+            this.btborrar.UseVisualStyleBackColor = false;
+            this.btborrar.Click += new System.EventHandler(this.btborrar_Click);
+            // 
+            // btigual
+            // 
+            this.btigual.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.btigual.Font = new System.Drawing.Font("Calisto MT", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btigual.Location = new System.Drawing.Point(201, 245);
+            this.btigual.Name = "btigual";
+            this.btigual.Size = new System.Drawing.Size(57, 100);
+            this.btigual.TabIndex = 17;
+            this.btigual.Text = "=";
+            this.btigual.UseVisualStyleBackColor = false;
+            this.btigual.Click += new System.EventHandler(this.btigual_Click);
+            // 
+            // txbcargar
+            // 
+            this.txbcargar.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txbcargar.Location = new System.Drawing.Point(15, 20);
+            this.txbcargar.Margin = new System.Windows.Forms.Padding(9, 10, 10, 10);
+            this.txbcargar.MaximumSize = new System.Drawing.Size(261, 60);
+            this.txbcargar.MinimumSize = new System.Drawing.Size(249, 47);
+            this.txbcargar.Name = "txbcargar";
+            this.txbcargar.Size = new System.Drawing.Size(249, 47);
+            this.txbcargar.TabIndex = 18;
+            this.txbcargar.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txbcargar.UseWaitCursor = true;
+            this.txbcargar.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Calculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.CadetBlue;
+            this.ClientSize = new System.Drawing.Size(274, 361);
+            this.Controls.Add(this.txbcargar);
+            this.Controls.Add(this.btigual);
+            this.Controls.Add(this.btborrar);
+            this.Controls.Add(this.btborrartodo);
+            this.Controls.Add(this.btcoma);
+            this.Controls.Add(this.bt0);
+            this.Controls.Add(this.bt9);
+            this.Controls.Add(this.bt8);
+            this.Controls.Add(this.bt7);
+            this.Controls.Add(this.bt6);
+            this.Controls.Add(this.bt5);
+            this.Controls.Add(this.bt4);
+            this.Controls.Add(this.bt3);
+            this.Controls.Add(this.bt2);
+            this.Controls.Add(this.bt1);
+            this.Controls.Add(this.btmultiplicar);
+            this.Controls.Add(this.btdividir);
+            this.Controls.Add(this.btresta);
+            this.Controls.Add(this.btsuma);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Calculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Caculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btsuma;
+        private System.Windows.Forms.Button btresta;
+        private System.Windows.Forms.Button btdividir;
+        private System.Windows.Forms.Button btmultiplicar;
+        private System.Windows.Forms.Button bt1;
+        private System.Windows.Forms.Button bt2;
+        private System.Windows.Forms.Button bt3;
+        private System.Windows.Forms.Button bt4;
+        private System.Windows.Forms.Button bt5;
+        private System.Windows.Forms.Button bt6;
+        private System.Windows.Forms.Button bt7;
+        private System.Windows.Forms.Button bt8;
+        private System.Windows.Forms.Button bt9;
+        private System.Windows.Forms.Button bt0;
+        private System.Windows.Forms.Button btcoma;
+        private System.Windows.Forms.Button btborrartodo;
+        private System.Windows.Forms.Button btborrar;
+        private System.Windows.Forms.Button btigual;
+        private System.Windows.Forms.TextBox txbcargar;
+    }
+}
+
---
+++ Evaluacion3/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Evaluacion3
+{
+    public partial class Calculadora : Form
+    {
+        public Calculadora()
+        {
+            InitializeComponent();
+        }
+        //variables necesarias
+        string operador = "";
+        double num1;
+        double num2;
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btborrar_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.TextLength ==1) txbcargar.Text = "0";
+            else txbcargar.Text = txbcargar.Text.Substring (0, txbcargar.TextLength - 1);
+        }
+
+        private void btborrartodo_Click(object sender, EventArgs e)
+        {
+            txbcargar.Text = "0";
+            num1 = 0;   
+            num2 = 0;
+            operador = "";
+
+        }
+
+        private void bt1_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "1";
+        }
+
+        private void bt2_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "2";
+        }
+
+        private void bt3_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "3";
+        }
+
+        private void bt4_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "4";
+        }
+
+        private void bt5_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "5";
+        }
+
+        private void bt6_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "6";
+        }
+
+        private void bt7_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "7";
+        }
+
+        private void bt8_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "8";
+        }
+
+        private void bt9_Click(object sender, EventArgs e)
+        {
+            if (txbcargar.Text == "0") txbcargar.Text = "";
+            txbcargar.Text = txbcargar.Text + "9";
+        }
+
+        private void bt0_Click(object sender, EventArgs e)
+        {
+            txbcargar.Text = txbcargar.Text + "0";
+        }
+
+        private void btcoma_Click(object sender, EventArgs e)
+        {
+            txbcargar.Text = txbcargar.Text + ",";
+        }
+
+        private void btsuma_Click(object sender, EventArgs e)
+        {
+            operador = "+";
+            num1= Convert.ToDouble(txbcargar.Text);
+            txbcargar.Text = "0";
+
+        }
+
+        private void btresta_Click(object sender, EventArgs e)
+        {
+            operador = "-";
+            num1 = Convert.ToDouble(txbcargar.Text);
+            txbcargar.Text = "0";
+        }
+
+        private void btdividir_Click(object sender, EventArgs e)
+        {
+            operador = "/";
+            num1 = Convert.ToDouble(txbcargar.Text);
+            txbcargar.Text = "0";
+        }
+
+        private void btmultiplicar_Click(object sender, EventArgs e)
+        {
+            operador = "*";
+            num1 = Convert.ToDouble(txbcargar.Text);
+            txbcargar.Text = "0";
+        }
+
+        private void btigual_Click(object sender, EventArgs e)
+        {
+            num2=Convert.ToDouble(txbcargar.Text);
+
+             switch (operador)
+             {
+                case "+":
+                    txbcargar.Text = $"{num1 + num2}";
+                break;
+
+                case "-":
+                    txbcargar.Text = $"{num1 - num2}";
+                    break;
+
+                case "/":
+                    txbcargar.Text = $"{num1 / num2}";
+                    break;
+
+                case "*":
+                    txbcargar.Text = $"{num1 * num2}";
+                    break;
+            }
+        }
+    }
+}
